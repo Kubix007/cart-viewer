aa6b5e8cb7da45959671bf3516858f3fb35417c1
Kubix007 <kuba.dolinski@hotmail.com>
2023-03-11 15:47:12 +0100

    Initialize project

---
+++ src/App.styles.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+import styled from "styled-components";
+
+export const App = styled(Stack)`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+  text-align: center;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import CartDetails from "./pages/CartDetails";
+import Dashboard from "./pages/Dashboard";
+import * as Styles from "./App.styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Styles.App>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/cartdetails" element={<CartDetails />} />
+        </Routes>
+      </Router>
+    </Styles.App>
   );
 }
 
